d6b469d842180a6b2f79dfc9a457c5562b2019ae
Erik Uecke <erikuecke@gmail.com>
2018-02-01 18:40:42 -0500

    Add scene with background to popover screen. Change scene camera to orthographicProjection, add have pipe as menu item to popover scene.

--- rampUp/Controller/RampPickerVCViewController.swift
+++ rampUp/Controller/RampPickerVCViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 
 class RampPickerVCViewController: UIViewController {
-
+    
     var sceneView: SCNView!
     var size: CGSize!
     
@@ -25,26 +25,44 @@ class RampPickerVCViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.frame = CGRect(origin: CGPoint.zero, size: size)
         sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         view.insertSubview(sceneView, at: 0)
         
+        let scene = SCNScene(named: "art.scnassets/ramps.scn")!
+        sceneView.scene = scene
+        
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection = true
+        scene.rootNode.camera = camera
+        
+        let obj = SCNScene(named: "art.scnassets/pipe.dae")
+        
+        if let node = obj?.rootNode.childNode(withName: "pipe", recursively: true) {
+            node.scale = SCNVector3Make(0.0022, 0.0022, 0.0022)
+            node.position = SCNVector3Make(-1, 0.7, -1)
+            scene.rootNode.addChildNode(node)
+        }
+        
+        
         preferredContentSize = size
         
+        
+        
     }
-
     
     
-
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
