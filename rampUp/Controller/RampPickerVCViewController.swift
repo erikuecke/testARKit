d20282bb37c03e8c408c42a24b79350f01d0b5f1
Erik Uecke <erikuecke@gmail.com>
2018-01-31 21:23:27 -0500

    Add popover for 3d model selector menu.

---
+++ rampUp/Controller/RampPickerVCViewController.swift
@@ -0,0 +1,50 @@
+//
+//  RampPickerVCViewController.swift
+//  rampUp
+//
+//  Created by Erik Uecke on 1/31/18.
+//  Copyright © 2018 Erik Uecke. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class RampPickerVCViewController: UIViewController {
+
+    var sceneView: SCNView!
+    var size: CGSize!
+    
+    init(size: CGSize) {
+        super.init(nibName: nil, bundle: nil)
+        self.size = size
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.frame = CGRect(origin: CGPoint.zero, size: size)
+        sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        view.insertSubview(sceneView, at: 0)
+        
+        preferredContentSize = size
+        
+    }
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
